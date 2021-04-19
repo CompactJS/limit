fc81cf6f6d20a9c67dd4ef25468c433348cb4b2a
Timo Bechtel <hi@timobechtel.com>
2021-04-19 16:36:27 +0200

    refactor: switch to typescript

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@compactjs/limit';
-/**
- * Limits value to max.
- * Values over max start with 0. Values below 0 begin with max.
- * @example limit(25, 24); // returns 1
- */
-export function limit(value: number, max: number): number;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-export const limit = (value, max) => {
+/**
+ * Limits value to max.
+ * Values over max start with 0. Values below 0 begin with max.
+ * @example limit(25, 24); // returns 1
+ */
+export function limit(value: number, max: number): number {
   if (typeof value !== 'number')
     throw 'limit: Parameter "value" needs to be a number.';
   if (typeof max !== 'number' || max <= 0)
     throw 'limit: Parameter "max" needs to be a positive number.';
   return (value % max) + (value < 0 ? max : 0);
-};
+}
